1730c23963b94a0947f943967b234b053ff2cea0
engahmedowdah <129810214+engahmedowdah@users.noreply.github.com>
2026-09-10 09:07:57 +0300

    feat: upload ecommerce project

--- frontend/admin/src/features/Admins/pages/AdminDetailsPage/AdminDetailsPage.tsx
+++ frontend/admin/src/features/Admins/pages/AdminDetailsPage/AdminDetailsPage.tsx
@@ -4,25 +4,38 @@ import type { IAdmin } from "../../../../shared/types/Admins/IAdmin.types";
 import { GetAdminByID } from "../../../../business/services";
 import { AdminDetails } from "../../components";
 import "./AdminDetailsPage.css";
+
 const AdminDetailsPage = () => {
   const { id } = useParams<{ id: string }>();
+
   const [admin, setAdmin] = useState<IAdmin | undefined>(undefined);
-  const [Loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    if (!id) return;
+    if (!id) {
+      setLoading(false);
+      return;
+    }
+
     GetAdminByID({ AdminID: id })
-      .then((res: IAdmin) => {
-        setAdmin(res);
-        setLoading(false);
+      .then((res) => {
+        if (res) {
+          setAdmin(res);
+        }
       })
       .catch(() => {
+        setAdmin(undefined);
+      })
+      .finally(() => {
         setLoading(false);
       });
   }, [id]);
+
   return (
     <div className="p-8 space-y-8 max-w-7xl mx-auto">
-      <AdminDetails admin={admin} Loading={Loading} />
+      <AdminDetails admin={admin} Loading={loading} />
     </div>
   );
 };
+
 export default AdminDetailsPage;
